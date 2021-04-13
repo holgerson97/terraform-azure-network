6c76c53d433ea25516bbbc7c52e9b7a725136d53
Nils Carstensen <nilscarstensen@live.com>
2021-04-13 09:08:03 +0200

    Fixed: Reference to resource rg

--- module/ddos_protection.tf
+++ module/ddos_protection.tf
@@ -4,7 +4,7 @@ resource "azurerm_network_ddos_protection_plan" "main" {
 
     name                = "${var.namespace}-ddos"
     location            = join("", azurerm_resource_group.main.*.location)
-    resource_group_name = join("", azurerm_resource_group.example.*.name)
+    resource_group_name = join("", azurerm_resource_group.main.*.name)
 
     depends_on = [
         azurerm_resource_group.main
