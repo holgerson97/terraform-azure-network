60726c4960ef41e3f506d53ab59b4104ac9ea08c
Nils Carstensen <nilscarstensen@live.com>
2021-04-13 09:08:59 +0200

    Add: Dynamic block for ddos and implemented VNET object in vnet resource

--- module/main.tf
+++ module/main.tf
@@ -9,20 +9,25 @@ resource "azurerm_resource_group" "main" {
 
 }
 
-resource "azurerm_virutal_network" "main" {
+resource "azurerm_virtual_network" "main" {
   
-    count = var.enabled == true ? 1 : 0
+    count = var.enabled == true ? lookup(var.vnet, "number_of_vnets") : 0
 
     name                = "${var.namespace}-vnet"
     location            = join("", azurerm_resource_group.main.*.location)
     resource_group_name = join("", azurerm_resource_group.main.*.name)
 
-    address_space       = var.vnet_address_space
-    dns_server          = var.dns_server
+    address_space       = element(lookup(var.vnet, "address_spaces"), count.index)
+    dns_servers         = var.dns_servers
 
-    ddos_protection_plan {
-        id     = join("", azurerm_network_ddos_protection_plan.main.*.id)
-        enable = var.ddos_protection
+    dynamic "ddos_protection_plan" {
+
+        for_each = var.ddos_protection == true ? [ "1" ] : []
+
+        content {
+            id     = join("", azurerm_network_ddos_protection_plan.main.*.id)
+            enable = var.ddos_protection
+        }
     }
 
     tags = var.tags
