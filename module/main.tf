6670e55b4e4080bc1d0b6562c2f003e43715d961
Nils Carstensen <nilscarstensen@live.com>
2021-04-14 21:08:29 +0200

    Add: Subnets in own resource

--- module/main.tf
+++ module/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "main" {
 
-    count = var.enabled == true ? 1 : 0
+    count = var.enabled ? 1 : 0
 
     name     = var.namespace
     location = var.location
@@ -11,18 +11,19 @@ resource "azurerm_resource_group" "main" {
 
 resource "azurerm_virtual_network" "main" {
   
-    count = var.enabled == true ? lookup(var.vnet, "number_of_vnets") : 0
+    count = var.enabled ? 1 : 0
 
-    name                = "${var.namespace}-${sum([count.index, 1])}"
-    location            = join("", azurerm_resource_group.main.*.location)
-    resource_group_name = join("", azurerm_resource_group.main.*.name)
+    name                  = "${var.namespace}-vnet"
+    location              = join("", azurerm_resource_group.main.*.location)
+    resource_group_name   = join("", azurerm_resource_group.main.*.name)
 
-    address_space       = element(lookup(var.vnet, "address_spaces"), count.index)
-    dns_servers         = var.dns_servers
+    address_space         = var.vnet_address_spaces
+    dns_servers           = var.dns_servers
+    vm_protection_enabled = var.vm_protection
 
     dynamic "ddos_protection_plan" {
 
-        for_each = var.ddos_protection == true ? [ "1" ] : []
+        for_each = var.ddos_protection == false ? [] : [ 1 ]
 
         content {
             id     = join("", azurerm_network_ddos_protection_plan.main.*.id)
@@ -35,4 +36,20 @@ resource "azurerm_virtual_network" "main" {
     depends_on = [
       azurerm_resource_group.main
     ]
+}
+
+resource "azurerm_subnet" "main" {
+
+    count = var.enabled ? length(var.subnet_address_spaces) : 0
+
+    name                 = "${var.namespace}-subnet-${sum([count.index, 1])}"
+    resource_group_name  = join("", azurerm_resource_group.main.*.name)
+    virtual_network_name = join("", azurerm_virtual_network.main.*.name)
+
+    address_prefixes = tolist([element(var.subnet_address_spaces, count.index)])
+
+    depends_on = [
+      azurerm_virtual_network.main
+    ]
+
 }
