f8b31da2fb0bc54cbd8a9f1b3a86661e5098e2d9
Nils Carstensen <nilscarstensen@live.com>
2021-04-20 15:02:49 +0200

    Add: Basic testing

--- unit-tests/go.mod
+++ unit-tests/go.mod
@@ -2,4 +2,7 @@ module azure-network
 
 go 1.13
 
-require github.com/gruntwork-io/terratest v0.32.22
+require (
+	github.com/gruntwork-io/terratest v0.32.22
+	github.com/stretchr/testify v1.4.0
+)
--- unit-tests/terraform_test.go
+++ unit-tests/terraform_test.go
@@ -2,22 +2,44 @@ package test
 
 import (
 	"testing"
-
+	
+	"github.com/stretchr/testify/assert"
+	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-func TestTerraformHelloWorldExampleApplyAndDestroy(t *testing.T) {
-	// Construct the terraform options with default retryable errors to handle the most common
-	// retryable errors in terraform testing.
+func TestInitAndApplyAndIdempotent(t *testing.T) {
+
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		// Set the path to the Terraform code that will be tested.
-		TerraformDir: "/home/nilscarstensen/repositorys/private/terraform-azure-network",
+
+		TerraformDir: "/home/nilscarstensen/repositorys/terraform-azure-network",
 	})
 
-	// Clean up resources with "terraform destroy" at the end of the test.
 	defer terraform.Destroy(t, terraformOptions)
 
-	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
-	// terraform.InitAndApply(t, terraformOptions)
 	terraform.InitAndApplyAndIdempotentE(t, terraformOptions)
+
+}
+
+func TestAzureResources(t *testing.T) {
+
+	subscriptionID := ""
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+
+		TerraformDir: "/home/nilscarstensen/repositorys/terraform-azure-network",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+	
+	t.Run("RG", func(t *testing.T) {
+
+		resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+
+		exists := azure.ResourceGroupExists(t, resourceGroupName, subscriptionID)
+		assert.True(t, exists, "Resource group does not exist")
+
+	})
 }
